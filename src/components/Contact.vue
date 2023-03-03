142f63c989b0029081d93498b5b484484031bc84
Tanya RB <trashid@jigsawcreate.com>
2023-03-03 20:29:02 +0000

    contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,46 @@
 <script setup lang="ts">
 import ContactCard from "./ContactCard.vue";
+import emailjs from "emailjs-com";
+import { ref } from "vue";
+
+const senderEmail = ref(null);
+const senderMessage = ref("");
+const senderName = ref("");
+const senderPhone = ref("");
+
+const enquirySubmitted = ref(true);
+
+async function sendEnquiry() {
+  if (senderEmail.value == null || senderPhone == null) {
+    alert(
+      "You must provide an email address and phone number to submit an enquiry."
+    );
+    return;
+  }
+
+  const formObject = {
+    senderEmail: senderEmail.value,
+    senderMessage: senderMessage.value,
+    senderPhone: senderPhone.value,
+    senderName: senderName.value,
+  };
+
+  emailjs
+    .send("BollywoodHoop", "template_9b0c5po", formObject, "X8scW6sUVAEqCH1Ap")
+    .then(
+      (result) => {
+        console.log("SUCCESS!", result);
+      },
+      (error) => {
+        console.log("FAILED...", error);
+      }
+    );
+
+  senderEmail.value = null;
+  senderName.value = "";
+  senderPhone.value = "";
+  senderMessage.value = "";
+}
 </script>
 
 <template>
@@ -32,19 +73,26 @@ import ContactCard from "./ContactCard.vue";
         </div>
         <div class="w-full px-4 lg:w-1/2 xl:w-5/12">
           <div class="relative rounded-lg bg-zinc-900 p-8 shadow-lg sm:p-12">
-            <form>
+            <div v-if="enquirySubmitted" class="py-20 text-white">
+              Thanks for your enquiry! We will be in touch.
+            </div>
+            <form v-else>
               <div class="mb-6">
                 <input
                   type="text"
                   placeholder="Your Name"
                   class="text-body-color focus:border-primary w-full rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                  v-model="senderName"
+                  name="senderName"
                 />
               </div>
               <div class="mb-6">
                 <input
-                  type="email"
+                  type="text"
                   placeholder="Your Email"
                   class="text-body-color focus:border-primary w-full rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                  v-model="senderEmail"
+                  name="senderEmail"
                 />
               </div>
               <div class="mb-6">
@@ -52,6 +100,8 @@ import ContactCard from "./ContactCard.vue";
                   type="text"
                   placeholder="Your Phone"
                   class="text-body-color focus:border-primary w-full rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                  v-model="senderPhone"
+                  name="senderPhone"
                 />
               </div>
               <div class="mb-6">
@@ -59,12 +109,22 @@ import ContactCard from "./ContactCard.vue";
                   rows="6"
                   placeholder="Your Message"
                   class="text-body-color focus:border-primary w-full resize-none rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                  v-model="senderMessage"
+                  name="senderMessage"
                 ></textarea>
+                <!-- <input
+                  placeholder="Your Message"
+                  class="text-body-color focus:border-primary w-full resize-none rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                  v-model="senderMessage"
+                  name="senderMessage"
+                  type="text"
+                /> -->
               </div>
               <div>
                 <button
                   type="submit"
                   class="mr-2 w-full rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800"
+                  @click="sendEnquiry"
                 >
                   Send Message
                 </button>

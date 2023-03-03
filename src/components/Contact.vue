a52610526923d1a38000874858f6d1932ba44e6c
Tanya RB <trashid@jigsawcreate.com>
2023-03-03 18:37:19 +0000

    contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,3 +1,74 @@
+<script setup lang="ts">
+import ContactCard from "./ContactCard.vue";
+</script>
+
 <template>
-  <div>Contact</div>
+  <!-- ====== Contact Section Start -->
+  <section
+    class="relative z-10 overflow-hidden bg-white p-14 lg:p-40 lg:py-[120px]"
+  >
+    <div class="container mx-auto">
+      <div class="-mx-4 flex flex-wrap lg:justify-between">
+        <div class="w-full px-4 lg:w-1/2 xl:w-6/12">
+          <div class="mb-12 max-w-[570px] lg:mb-0">
+            <h2
+              class="text-dark mb-6 text-[32px] font-bold uppercase sm:text-[40px] lg:text-[36px] xl:text-[40px]"
+            >
+              LET'S CHAT!
+            </h2>
+            <p class="text-body-color mb-14 text-base leading-relaxed">
+              Contact me to find out more about packages, rates and
+              availability. We can talk through your requirements and see if we
+              can create a package bespoke to your needs.
+            </p>
+            <ContactCard :icon="'phone'"></ContactCard>
+            <ContactCard :icon="'email'"></ContactCard>
+            <ContactCard :icon="'instagram'"></ContactCard>
+          </div>
+        </div>
+        <div class="w-full px-4 lg:w-1/2 xl:w-5/12">
+          <div class="relative rounded-lg bg-red-900 p-8 shadow-lg sm:p-12">
+            <form>
+              <div class="mb-6">
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  class="text-body-color focus:border-primary w-full rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                />
+              </div>
+              <div class="mb-6">
+                <input
+                  type="email"
+                  placeholder="Your Email"
+                  class="text-body-color focus:border-primary w-full rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                />
+              </div>
+              <div class="mb-6">
+                <input
+                  type="text"
+                  placeholder="Your Phone"
+                  class="text-body-color focus:border-primary w-full rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                />
+              </div>
+              <div class="mb-6">
+                <textarea
+                  rows="6"
+                  placeholder="Your Message"
+                  class="text-body-color focus:border-primary w-full resize-none rounded border border-[f0f0f0] py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                ></textarea>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  class="bg-primary border-primary w-full rounded border p-3 text-white transition hover:bg-opacity-90"
+                >
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
---
+++ src/components/ContactCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import IconPhone from "../icons/IconPhone.vue";
+import Email2 from "../icons/Email2.vue";
+import InstagramBlack from "../icons/InstagramBlack.vue";
+const props = defineProps<{ icon: string }>();
+
+let cardText = "";
+let cardTitle = "";
+
+switch (props.icon) {
+  case "phone":
+    cardTitle = "Phone / Whatsapp";
+    cardText = "+44 7392 260 579";
+    break;
+  case "email":
+    cardTitle = "Email";
+    cardText = "bollywoodhoop@outlook.com";
+    break;
+  case "instagram":
+    cardTitle = "Instagram";
+    cardText = "@bollywoodaerial";
+    break;
+}
+</script>
+
+<template>
+  <div class="mb-1 flex h-14 w-3/4 flex-row sm:h-20 sm:w-2/3">
+    <IconPhone v-if="icon == 'phone'" class="h-1/3 w-1/3"></IconPhone>
+    <Email2 v-if="icon == 'email'" class="h-1/3 w-1/3"></Email2>
+    <InstagramBlack
+      v-if="icon == 'instagram'"
+      class="h-1/3 w-1/3"
+    ></InstagramBlack>
+    <div class="w-2/3">
+      <h4 class="text-dark mb-1 text-sm sm:block sm:text-xl">
+        {{ cardTitle }}
+      </h4>
+      <p class="text-body-color sm-text-xl text-base text-sm font-bold">
+        {{ cardText }}
+      </p>
+    </div>
+  </div>
+</template>

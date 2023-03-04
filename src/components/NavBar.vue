a9e77d235c9b54c6964b70c27bbf0e07fabdecd1
Tanya RB <trashid@jigsawcreate.com>
2023-03-04 16:32:04 +0000

    nav bar fix style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,13 +4,22 @@ import { onClickOutside } from "@vueuse/core";
 import MenuButton from "../icons/MenuButton.vue";
 import MenuButtonOpen from "../icons/MenuButton.vue";
 import InstagramLogo from "../icons/InstagramLogo.vue";
-import Email from "../icons/Email.vue";
+import InstagramBlack from "../icons/InstagramBlack.vue";
+import Email2 from "../icons/Email2.vue";
 import { useDisplay } from "../assets/composables/use.display";
 import { text } from "stream/consumers";
 import NavItem from "./NavItem.vue";
 import { useRouter, useRoute, RouteLocationNormalizedLoaded } from "vue-router";
 
 const { openInstagram, emailMe, activePage } = useDisplay();
+
+const navClass = computed<string>(() => {
+  const isHomePageOpen = activePage.value == "Home";
+  const navClass = isHomePageOpen
+    ? "absolute z-20 w-screen"
+    : "absolute z-20 w-screen bg-stone-900";
+  return navClass;
+});
 
 // MOBILE STUFF
 
@@ -27,34 +36,12 @@ const componentWrapperElement = ref<HTMLElement>();
 onClickOutside(componentWrapperElement, () => {
   toggleMobileMenu(false);
 });
-
-const selectedTabClass =
-  "rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white";
-
-const navFontStyleLarge = {
-  "font-family": "Impact",
-  color: "red",
-};
-
-const navFontStyleSmall = {
-  "font-family": "Arial",
-  color: "red",
-};
-
-const navClass = computed<string>(() => {
-  const mediumSizeClass = "px-4 py-2 text-base";
-  const isHomePageOpen = activePage.value == "Home";
-  const navClass = isHomePageOpen
-    ? "absolute z-20 w-screen"
-    : "absolute z-20 w-screen bg-zinc-900";
-  return navClass;
-});
 </script>
 
 <template>
   <nav :class="navClass">
     <div class="mx-auto px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
+      <div class="relative flex h-12 items-center justify-between">
         <!-- LOGO -->
         <div class="flex flex-shrink-0 items-center">
           <!-- <img
@@ -63,7 +50,7 @@ const navClass = computed<string>(() => {
             alt="The Bollywood Hoop Artist"
           /> -->
           <img
-            class="hidden h-24 md:block lg:block"
+            class="hidden h-12 md:block lg:block"
             src="../assets/svg/nobackground-amazon-CROPPED.png"
             alt="The Bollywood Hoop Artist"
           />
@@ -73,7 +60,7 @@ const navClass = computed<string>(() => {
           <!-- NAV ITEMS -->
 
           <div class="sm:ml-6 sm:block">
-            <div class="flex justify-center lg:space-x-4">
+            <div class="flex justify-center lg:space-x-6">
               <NavItem
                 :router-link-to="'HomePage'"
                 :nav-title="'Home'"
@@ -99,12 +86,12 @@ const navClass = computed<string>(() => {
           class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
           <div class="relative ml-3">
-            <div>
+            <div class="grid grid-cols-2">
               <button @click="openInstagram">
-                <InstagramLogo class="h-7 w-7 sm:h-8 sm:w-8"></InstagramLogo>
+                <InstagramBlack class="h-6 w-6 sm:h-7 sm:w-7"></InstagramBlack>
               </button>
               <button @click="emailMe">
-                <Email class="mb-0.5 h-4 sm:mb-1 sm:ml-2 sm:h-6 sm:w-6"></Email>
+                <Email2 class="h-4 sm:h-6 sm:w-6"></Email2>
               </button>
             </div>
           </div>
--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,24 +1,46 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useDisplay } from "../assets/composables/use.display";
 const props = defineProps<{ routerLinkTo: string; navTitle: string }>();
+
 const textColor = "white";
 const navFontStyle = {
   color: textColor,
-  fontFamily: "Helvetica, sans-serif",
+  fontFamily: "Didot",
+  fontWeight: 100,
 };
 
 const { activePage } = useDisplay();
 
 function updateActivePage(page: string) {
   activePage.value = page;
+  console.log(activePage.value);
 }
+
+const navTextClass = computed<string>(() => {
+  const old = "sm:text-m rounded-md px-3 py-3 text-sm font-medium ";
+  const baseClass =
+    "text-black hover:bg-black hover:text-white font-sans font-thin text-xs rounded-md p-2 tracking-widest";
+  const homeClass =
+    baseClass + " text-black hover:bg-stone-800 hover:text-white";
+  const otherPagesClass =
+    baseClass + " text-white hover:bg-stone-200 hover:text-black";
+
+  const activePageClass = baseClass + " text-white bg-stone-500";
+  const isHomePageOpen = activePage.value == "Home";
+  const textClass = isHomePageOpen
+    ? homeClass
+    : props.navTitle == activePage.value
+    ? activePageClass
+    : otherPagesClass;
+  return textClass;
+});
 </script>
 
 <template>
   <router-link
     :to="{ name: routerLinkTo }"
-    class="sm:text-m rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-black hover:text-white"
-    :style="navFontStyle"
+    :class="navTextClass"
     @click="updateActivePage(navTitle)"
     >{{ navTitle }}</router-link
   >
